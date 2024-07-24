70d5a3d435ebf077ee7e728ac7ddc2fa7364a21f
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2024-07-24 17:55:24 -0400

    First Version

---
+++ 8_2_Numeric_Examples.sql
@@ -0,0 +1,22 @@
+-- SITE: https://sqlzoo.net/wiki/NSS_Tutorial
+
+-- 1. Show the the percentage who STRONGLY AGREE
+
+
+-- 2. Show the institution and subject where the score is at least 100 for question 15.
+
+
+-- 3. Show the institution and score where the score for '(8) Computer Science' is less than 50 for question 'Q15'
+
+
+-- 4. Show the subject and total number of students who responded to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'.
+
+
+-- 5. Show the subject and total number of students who A_STRONGLY_AGREE to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'.
+
+
+-- 6. Show the percentage of students who A_STRONGLY_AGREE to question 22 for the subject '(8) Computer Science' show the same figure for the subject '(H) Creative Arts and Design'.
+
+
+-- 8. Show the institution, the total sample size and the number of computing students for institutions in Manchester for 'Q01'.
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# SQK QUERIES PRACTICE
+The exercises are from [SLQZoo](https://sqlzoo.net/wiki/SQL_Tutorial), to practice SQL queries.
+These solutions only apply the functions taken in each tutorial topic.
+
+**Include a Template Branch so you can complete and share the entire tutorial.**
+
+*Information: The site's exercise database appears to be temporarily down.*
