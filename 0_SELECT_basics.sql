720cd6f35b52480b9f6bd9cf98cdc34d90d2e7de
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2024-07-24 18:15:10 -0400

    Add Template

---
+++ 0_SELECT_basics.sql
@@ -0,0 +1,12 @@
+-- SITE: https://sqlzoo.net/wiki/SELECT_basics
+
+-- 1. Modify it to show the population of Germany  
+
+
+-- 2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+
+
+-- 3. Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
+
+
+-- QUIZ: https://sqlzoo.net/wiki/SELECT_Quiz
---
+++ 4_SELECT_within_SELECT.sql
@@ -0,0 +1,31 @@
+-- SITE: https://sqlzoo.net/wiki/SELECT_within_SELECT_Tutorial
+
+-- 1. List each country name where the population is larger than that of 'Russia'.
+
+
+-- 2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+
+
+-- 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+
+-- 4. Which country has a population that is more than United Kingdom but less than Germany? Show the name and the population.
+
+
+-- 5. Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+
+
+-- 6. Which countries have a GDP greater than every country in Europe?
+
+
+-- 7. Find the largest country (by area) in each continent, show the continent, the name and the area.
+
+
+-- 8. Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+
+
+-- 9. Some countries have populations more than three times that of all of their neighbours (in the same continent). Give the countries and continents.
+
+
+
+-- QUIZ: https://sqlzoo.net/wiki/Nested_SELECT_Quiz
